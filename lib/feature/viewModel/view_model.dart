edee70db3f98d7837b094edb163fb47b294cb430
Firas-Zn <firas.hmzn@hotmail.com>
2022-12-20 10:35:51 +0300

    finaly_from_file_storage

--- lib/feature/viewModel/view_model.dart
+++ lib/feature/viewModel/view_model.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:just_audio/just_audio.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
